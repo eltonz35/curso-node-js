68b127a30ee111e9c5484eed87cf1058d9e415d8
Elthon <eltonsilva.mecatronic@gmail.com>
2022-09-12 18:26:58 -0300

    criando calbacks

--- aula02/callbacks/ex1.js
+++ aula02/callbacks/ex1.js
@@ -5,7 +5,7 @@ function obterCarro(callback) {
     setTimeout(() => {
         return callback(null, {
             id: 1,
-            modelo: uno 
+            modelo: "uno" 
         })
     }, 1000);
 }
@@ -13,7 +13,7 @@ function obterCarro(callback) {
 function obterMarca(idVeiculo, callback) {
     setTimeout(() => {
         return callback(null, {
-            marca: fiat 
+            marca: "fiat" 
         })
     }, 2000);
 }
@@ -21,7 +21,34 @@ function obterMarca(idVeiculo, callback) {
 function obterTipo(idVeiculo, callback) {
     setTimeout(() => {
         return callback(null, {
-            tipo: hatch
+            tipo: "hatch"
         })
     }, 2000);
 }
+
+//Calbacks
+
+obterCarro(function resolverCarro(error, veiculo) {
+    if(error) {
+        console.error("Erro no modelo do veículo!", error);
+        return;
+    }
+    obterMarca(veiculo.id ,function resolverMarca(error1, marca) {
+        if(error1) {
+            console.error("Erro na marca do veículo!", error1);
+            return;
+        }
+        obterTipo(veiculo.id ,function resolverTipo(error2, tipo) {
+            if(error2) {
+                console.error("Erro no tipo do veículo!", error2);
+                return;
+            }
+
+            console.log(`
+            Modelo: ${veiculo.modelo}
+            Marca:  ${marca.marca}
+            Tipo:   ${tipo.tipo} 
+            `);
+        })
+    })
+})
