12b9c0775888dade851408bc19ecabb6cd24de54
Elthon <eltonsilva.mecatronic@gmail.com>
2022-09-23 22:04:20 -0300

    padrao de teste

---
+++ modulo04/cli/teste.js
@@ -0,0 +1,23 @@
+const {
+    deepEqual,
+    ok
+} = require('assert')
+
+const DEFAULT_ITEM_CADASTRAR = {
+    nome: 'Flash',
+    poder: 'Speed',
+    id: 1
+}
+
+describe('Suite de manipulação de Herois', () => {
+    it('deve pesquisar um heroi usando arquivos', async () => {
+        const expected = DEFAULT_ITEM_CADASTRAR
+
+        ok(null, expected)
+    })
+    // it('deve cadastrar um heroi, usando arquivos', async () => {
+    //     const expected = DEFAULT_ITEM_CADASTRAR
+    //     //
+    //     ok(null, expected)
+    // })
+})
